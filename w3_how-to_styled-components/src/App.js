cc9f5d907a6ddedf4dd3fd6197e0a400965e31b9
valerio sgura <valeriosgura88@gmail.com>
2021-10-27 12:18:44 +0200

    Creazione progetto, GlobalStyle

---
+++ w3_how-to_styled-components/src/App.js
@@ -0,0 +1,12 @@
+import GlobalStyle from "./globalStyles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
---
+++ w3_how-to_styled-components/src/globalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+* {
+    font-family: 'DM Sans';
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: grey;
+  }
+`;
+
+export default GlobalStyle
+
