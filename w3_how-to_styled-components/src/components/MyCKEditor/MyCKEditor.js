a576f1d531572879cd4e7616c5806219e723c497
valerio sgura <valeriosgura88@gmail.com>
2021-11-02 19:14:59 +0100

    MyCKEditor - onReady, onFocus, onBlur

--- w3_how-to_styled-components/src/components/MyCKEditor/MyCKEditor.js
+++ w3_how-to_styled-components/src/components/MyCKEditor/MyCKEditor.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
+import { CKEditor, CKEditorContext } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import parse from 'html-react-parser';
 import {
@@ -23,11 +23,33 @@ const MyCKEditor = () => {
         />
       </EditorContainer>
       <div>
-        <h2>Content</h2>
+        <h2>Content: </h2>
         <p>{parse(text)}</p>
       </div>
+
+      <EditorContainer>
+        <CKEditor
+          editor={ClassicEditor}
+          data="<p>Hello from CKEditor 5!</p>"
+          onReady={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor);
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData();
+            console.log({ event, editor, data });
+          }}
+          onBlur={(event, editor) => {
+            console.log('Blur.', editor);
+          }}
+          onFocus={(event, editor) => {
+            console.log('Focus.', editor);
+          }}
+        />
+      </EditorContainer>
     </>
   )
-}
+};
 
 export default MyCKEditor
+
